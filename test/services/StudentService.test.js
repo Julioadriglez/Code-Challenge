722619a292231be0fccf593b9de9e84e1469ec16
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-13 18:07:40 -0500

    Se realiso segunda prueba en StudentService.test.js

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,9 +1,14 @@
 const StudentService = require("../../lib/services/StudentService");
 
 describe("Pruebas de unidad para StudentService", () => {
-    test('Requerimiento 1. consulta de estudiantes', () => {
+    test('Requerimiento 1. Consulta de estudiantes', () => {
         const student = [{"name" : "jose"}, {"name" : "luis"}, {"name" : "isac"}];
-        const studentFilter = StudentService.students(student)
-        expect(studentFilter[0].name).toBe("jose")
+        const allStudent = StudentService.students(student)
+        expect(allStudent[0].name).toBe("jose")
+    });
+    test("Requerimiento 2. Consulta de emails por sertificacion", () => {
+        const  student = [{"name": "jose", "haveCertification": true}]
+        const studenfilter = StudentService.studentsFilter(student)
+        expect(studenfilter.haveCertification).toBe(true)
     });
 })
