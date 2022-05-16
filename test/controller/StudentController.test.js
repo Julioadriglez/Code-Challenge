9c098d03d641d75a26a7858da0705996439f36fe
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-16 18:39:20 -0500

    se gernera archivo Reader,js

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -8,7 +8,7 @@ describe("pruebas de unidad para StudentController", () => {
     });
     test("Requerimiento 2. Consulta de emails por certificación", () => {
         const student = [{"name": "jose", "haveCertification": true, "email": "Todd@visualpartnership.xyz"}];
-        const studenfilter = StudentController.getstudentFilter(student, true);
+        const studenfilter = StudentController.getStudentFilter(student, true);
         expect(studenfilter[0]).toBe("Todd@visualpartnership.xyz");
     });
 });
