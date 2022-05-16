a661608f143c30b3368e7a55b5d05b03ebcecd08
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-16 18:28:51 -0500

    creacion de la prueba 2

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -6,4 +6,9 @@ describe("pruebas de unidad para StudentController", () => {
         const allStudent = StudentController.getStudents(student);
         expect(allStudent[0].name).toBe("jose");
     });
+    test("Requerimiento 2. Consulta de emails por certificación", () => {
+        const student = [{"name": "jose", "haveCertification": true, "email": "Todd@visualpartnership.xyz"}];
+        const studenfilter = StudentController.getstudentFilter(student, true);
+        expect(studenfilter[0]).toBe("Todd@visualpartnership.xyz");
+    });
 });
