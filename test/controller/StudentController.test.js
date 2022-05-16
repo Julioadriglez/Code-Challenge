f79fb0e91ba0bb7f1fdc797602dbadf48238ec46
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-16 18:26:58 -0500

    paso primera prueba

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -1,4 +1,3 @@
-const { TestScheduler } = require("jest")
 const StudentController = require("../../lib/controller/StudentController")
 
 describe("pruebas de unidad para StudentController", () => {
@@ -6,4 +5,5 @@ describe("pruebas de unidad para StudentController", () => {
         const student = [{"name" : "jose"}, {"name" : "luis"}, {"name" : "isac"}];
         const allStudent = StudentController.getStudents(student);
         expect(allStudent[0].name).toBe("jose");
-})
+    });
+});
