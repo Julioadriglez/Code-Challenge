5d1257e68da4063097f793ab9621a6a73f561a71
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-20 18:46:09 -0500

    preuba 2 pasa

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -2,8 +2,13 @@ const { TestScheduler } = require("jest");
 const StudentController = require("../../lib/controller/StudentController");
 
 describe("Pruebas a StudenController", () => {
-    test("getStudents no regrese Undefined", () => {
+    test("Prueba 1. getStudents no regrese Undefined", () => {
         const allStudents = StudentController.getStudents()
         expect(allStudents).not.toBeUndefined();
     });
+    test("Prueba 2.getStudentFilter no regrese Undefined", () => {
+        const allStudents = StudentController.getStudentFilter();
+        expect(allStudents).not.toBeUndefined();
+    });
+
 });
