a3d1ab1af6c49062a26fbd145e14648f819b74bd
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-20 17:17:40 -0500

    Se agrego prueba en Reader.test.js y paso

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,9 @@
+
+const Reader = require("../../lib/utils/Reader")
+
+describe("Prueba a Reader ", () => {
+    test("no regrese Undefined", () => {
+        const students = Reader.readJsonFile("visualpartners.json");
+        expect(students).not.toBeUndefined();
+    });
+});
